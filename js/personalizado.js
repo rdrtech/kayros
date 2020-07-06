2eb66eae2ee1af33e4f2f593c70b8289969bda11
Ricardo Alves <ricbatera@bol.com.br>
2020-07-06 19:01:49 -0300

    integrado pagina de contato com a index

---
+++ js/personalizado.js
@@ -0,0 +1,14 @@
+document.querySelectorAll('.click-menu').forEach(link =>{
+    const conteudo = document.getElementById('conteudo');
+
+    link.onclick = function(e){
+        e.preventDefault();
+        fetch(link.href)
+            .then(resp => resp.text())
+            .then(html => conteudo.innerHTML = html)
+    }
+});
+
+function msg() {
+    alert("Programar essa parte do envio");
+}
